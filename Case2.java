ddad5095f97619da733788844b1dab401b7e6646
Shivatarun <99860346+ShivaTarun0909@users.noreply.github.com>
2022-02-28 15:37:27 +0530

    Add files via upload

---
+++ Case2.java
@@ -0,0 +1,30 @@
+package Assignment3;
+import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class Case2 {
+    public static void writing() {
+        try {
+
+            File statText = new File("E:/Java/Reference/bin/images/statsTest.txt");
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);
+            Writer w = new BufferedWriter(osw);
+            w.write("POTATO!!!");
+            w.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the file statsTest.txt");
+        }
+    }
+
+    public static void main(String[] args) {
+        Case2 write = new Case2();
+        Case2.writing();
+    }
+
+}
---
+++ Casestudy2.java
@@ -0,0 +1,27 @@
+package Assignment3;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Casestudy2 {
+
+        public static void main(String [] args) {
+            FileWriter fw= null;
+            File file =null;
+            try {
+                file=new File("WriteFile.txt");
+                if(!file.exists()) {
+                    file.createNewFile();
+                }
+                fw = new FileWriter(file);
+                fw.write("This is an string written to a file");
+                fw.flush();
+                fw.close();
+                System.out.println("File written Succesfully");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
